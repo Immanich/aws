68cdcdc0edb9921861da25924f94729a93343875
Immanich <immanich7@gmail.com>
2024-09-02 09:01:11 +0800

    heyow

---
+++ src/constants/common.ts
@@ -0,0 +1,15 @@
+export const NAME = 'Lord Immanuel S. Juanich';
+
+// ABOUT PAGE
+export const ABOUT_TEXT_1 =
+  'Skilled intern specializing in writing, testing, debugging, and maintaining software code to support the development of functional applications and systems. Gaining expertise in programming languages, algorithms, data structures, and software development methodologies.';
+export const ABOUT_TEXT_2 =
+  'Tasked with developing and maintaining web applications, from frontend to backend, and collaborating closely with cross-functional teams to design, implement, and deploy scalable solutions aligned with project objectives.';
+// export const ABOUT_TEXT_3 =
+//   'Versatile intern with skills in both frontend and backend development, capable of contributing to the creation of complete web applications from start to finish. Gaining strong knowledge of various programming languages, frameworks, and databases.';
+
+// RESUME PAGE
+export const RESUME_TEXT_1 =
+  'Software Developer Intern with 4 years of academic and practical experience in building websites and web applications using modern JavaScript tools and frameworks. Developing a solid understanding of both front-end and back-end technologies and database management.';
+export const RESUME_TEXT_2 =
+  'Currently a 4th-year BSIT student, continuing to deepen expertise in software development and stay updated with the latest industry trends.';
